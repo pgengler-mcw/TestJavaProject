ecc07485e602e3dbb73c0800dd09d0a6c7e7df5c
pgengler <pgengler@CIBMTR-182.mcwcorp.net>
2014-10-16 12:24:50 -0500

    This is the initial load of this project to GitHub

---
+++ TestJavaProjectGH/src/org/cibmtr/main/TestJavaClass.java
@@ -0,0 +1,13 @@
+package org.cibmtr.main;
+
+public class TestJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double pi = 3.1415926535;
+		System.out.println("This is a test Java class + pi: " + pi);
+		// Class Update #1	
+		System.out.println("This is the first update of this Java class");
+	}
+
+}
